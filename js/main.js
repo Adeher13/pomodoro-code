374416c821cdf1c27d4c42c634b5dca2c61e59e1
Adeher <adeherlucas@gmail.com>
2023-02-08 11:18:05 -0300

    Conectar o ativador do cronometro com a função de criar cronometro

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
 const segundosPomodoro =4000//25*60*1000
+const disparador = document.querySelector('#disparador')
 
-setTimeout(() => {
+disparador.addEventListener('click', () =>{
+   console.log("disparador ativado.")
+   setTimeout(() => {
    console.log("Acabou o pomodoro!") 
 },4000 );
+})
+
+
